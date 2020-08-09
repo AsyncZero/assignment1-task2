4b22a8abb08d49efd2b68587d2ff146415eae708
Mitch <mboyd@outlook.com.au>
2020-08-09 12:55:56 +1000

    Image Path Read From Database Function

--- public/env.js
+++ public/env.js
@@ -1,3 +1,34 @@
+const retrieveImages = function () {
+  $.get("/images", (images) => {
+    console.log(images);
+    $("#images").empty();
+    images.forEach(function (image) {
+      console.log(image);
+      // $("#images").append('<div class="row image" >' + image.image + "</div>");
+      $("#images").append(
+        `<img class="materialboxed" width="650" src="${image.image}"></img>`
+      );
+    });
+  });
+};
+
 $(document).ready(function () {
   console.log("Ready");
+
+  $("#btnMessage").click(() => {
+    let image = $("#imageBox").val();
+    let message = $("#messageBox").val();
+    let data = {
+      image,
+      message,
+    };
+    $.get("/imageinsert", data, () => {
+      $("#messageBox").val("");
+      $("#imageBox").val("");
+    });
+  });
+
+  setInterval(() => {
+    retrieveImages();
+  }, 1000);
 });
